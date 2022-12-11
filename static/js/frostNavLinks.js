b102babbe0f1e96c31a493e09d9e2a352d5e4b5f
NiuBoss123 <i@niuboss123.com>
2022-12-11 09:59:44 +0800

    2022-12-11-2

--- static/js/frostNavLinks.js
+++ static/js/frostNavLinks.js
@@ -70,5 +70,353 @@ var frostNavLinks = {
                 },
             ]
         },
+        {
+            title: '博客 / 社区 / 社交 / 通讯',
+            icon: 'el-icon-chat-line-round',
+            sub: [
+                {
+                    title: '博客',
+                    links: [
+                        {
+                            title: '4SSH',
+                            link: 'https://4ssh.cn/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'Frost-ZX',
+                            desc: '一个分享技术、资源，记录学习与生活的博客。',
+                            link: 'https://blog.frost-zx.top/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'haojunmei',
+                            link: 'https://www.haojunmei.top/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'Kenvix\'s Blog',
+                            link: 'https://kenvix.com/',
+                        },
+                        {
+                            title: 'Luminous\' Home',
+                            link: 'https://luotianyi.vc/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'UNKNOW date',
+                            link: 'https://n-a.date/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'zgcwkj',
+                            link: 'https://blog.zgcwkj.cn/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '安和 Blog',
+                            link: 'https://lolicorn.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '枫叶博客',
+                            link: 'https://www.myitmx.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '老D博客',
+                            link: 'https://laod.cn/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '阮一峰的网络日志',
+                            link: 'http://www.ruanyifeng.com/blog/',
+                            update: '2021-06-20'
+                        },
+                        {
+                            title: '樱花庄的白猫',
+                            link: 'https://2heng.xin/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '云梦',
+                            link: 'https://www.htcp.net/',
+                            update: '2021-02-07'
+                        }
+                    ]
+                },
+                {
+                    title: '论坛 / 社区',
+                    links: [
+                        {
+                            title: 'PC426',
+                            link: 'https://www.pc426.com/',
+                        },
+                        {
+                            title: 'SegmentFault 思否',
+                            link: 'https://segmentfault.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'Stack Overflow',
+                            link: 'https://stackoverflow.com/',
+                            update: '2021-06-20'
+                        },
+                        {
+                            title: 'V2EX',
+                            link: 'https://www.v2ex.com/',
+                            update: '2021-02-08'
+                        },
+                        {
+                            title: '百度贴吧',
+                            link: 'https://tieba.baidu.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '地铁族',
+                            link: 'http://www.ditiezu.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '豆瓣',
+                            link: 'https://www.douban.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '捷径社区',
+                            desc: '发现和分享 Siri 捷径、快捷指令',
+                            link: 'https://sharecuts.cn/',
+                            update: '2021-06-04'
+                        },
+                        {
+                            title: '开源中国',
+                            link: 'https://www.oschina.net/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '蓝点网',
+                            desc: '给你感兴趣的内容',
+                            link: 'https://www.landian.vip/',
+                            update: '2021-11-07'
+                        },
+                        {
+                            title: '码云社',
+                            desc: '用代码改变世界',
+                            link: 'https://www.codeseeding.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '天空之城',
+                            desc: '全球航拍爱好者和专业摄影师的作品社区',
+                            link: 'https://www.skypixel.com/',
+                            update: '2021-06-05'
+                        },
+                        {
+                            title: '天涯社区（天涯论坛）',
+                            link: 'https://bbs.tianya.cn/',
+                            update: '2021-05-28'
+                        },
+                        {
+                            title: '知乎',
+                            link: 'https://www.zhihu.com/',
+                            update: '2021-02-07'
+                        }
+                    ]
+                },
+                {
+                    title: '社交',
+                    links: [
+                        {
+                            title: 'Instagram',
+                            link: 'https://www.instagram.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'QQ空间',
+                            link: 'https://qzone.qq.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'Twitter',
+                            link: 'https://twitter.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '新浪微博',
+                            link: 'https://weibo.com/',
+                            update: '2021-02-07'
+                        }
+                    ]
+                },
+                {
+                    title: '通讯',
+                    links: [
+                        {
+                            title: 'Discord',
+                            link: 'https://discordapp.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '开黑啦',
+                            link: 'https://www.kaiheila.cn/',
+                            update: '2021-04-07'
+                        },
+                        {
+                            title: '云短信',
+                            desc: '在线短信接收',
+                            link: 'https://www.materialtools.com/',
+                            update: '2021-02-07'
+                        }
+                    ]
+                },
+                {
+                    title: '邮箱',
+                    links: [
+                        {
+                            title: '126邮箱',
+                            link: 'http://mail.126.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'Outlook',
+                            link: 'https://outlook.live.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: 'QQ邮箱',
+                            link: 'https://mail.qq.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '网易邮箱',
+                            link: 'https://mail.163.com/',
+                            update: '2021-02-07'
+                        },
+                        {
+                            title: '新浪邮箱',
+                            link: 'https://mail.sina.com.cn/',
+                            update: '2021-02-07'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            title: '出行 / 服务 / 生活',
+            icon: 'el-icon-bicycle',
+            sub: [
+                {
+                    title: '出行',
+                    links: [
+                        {
+                            title: '地铁通（MetroMan）',
+                            link: 'http://www.metroman.cn/',
+                            update: '2021-04-16'
+                        },
+                        {
+                            title: '中国铁路 12306',
+                            link: 'https://www.12306.cn/',
+                            update: '2021-02-08'
+                        }
+                    ],
+                    sub: [
+                        {
+                            title: '轨道交通',
+                            links: [
+                                {
+                                    title: '北京地铁',
+                                    link: 'https://www.bjsubway.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '成都地铁',
+                                    link: 'http://www.chengdurail.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '广州地铁',
+                                    link: 'http://www.gzmtr.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '杭州地铁',
+                                    link: 'http://www.hzmetro.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '南京地铁',
+                                    link: 'http://www.njmetro.com.cn/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '南宁地铁',
+                                    link: 'http://www.nngdjt.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '青岛地铁',
+                                    link: 'http://www.qd-metro.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '上海地铁',
+                                    link: 'http://www.shmetro.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '沈阳地铁',
+                                    link: 'http://www.symtc.com/',
+                                    update: '2021-02-08'
+                                },
+                                {
+                                    title: '深圳地铁',
+                                    link: 'http://www.szmc.net/',
+                                    update: '2021-02-08'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: '服务',
+                    links: [
+                        {
+                            title: '公安部“互联网+政务服务”平台',
+                            link: 'https://zwfw.mps.gov.cn/',
+                            update: '2021-06-04'
+                        }
+                    ]
+                },
+                {
+                    title: '旅游',
+                    links: [
+                        {
+                            title: '飞猪旅行',
+                            link: 'https://www.alitrip.com/',
+                            update: '2021-02-08'
+                        },
+                        {
+                            title: '途牛',
+                            link: 'http://www.tuniu.com/',
+                            update: '2021-02-08'
+                        },
+                        {
+                            title: '携程旅行网',
+                            link: 'https://www.ctrip.com/',
+                            update: '2021-02-08'
+                        }
+                    ]
+                },
+                {
+                    title: '生活',
+                    links: [
+                        {
+                            title: '绿色呼吸',
+                            link: 'http://www.pm25.com/',
+                            update: '2021-02-08'
+                        }
+                    ]
+                }
+            ]
+        },
     ]
 };
